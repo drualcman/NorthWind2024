3f5661ef53087d37eac27843bf5a6ff0c089ef5d
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-07 11:18:07 +0800

    agregado el caso de uso createorder

---
+++ NorthWind.Sales.Backend.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthWind.Sales.Backend.UseCases.CreateOrder;
+internal class CreateOrderInteractor(ICommandsRepository Repository, ICreateOrderOutputPort OutputPort) : ICreateOrderInputPort
+{
+    public async Task Handle(CreateOrderDto orderDto)
+    {
+        OrderAgregate order = OrderAgregate.From(orderDto);
+        await Repository.CreateOrder(order);
+        await Repository.SaveChanges();
+        await OutputPort.Handle(order);
+    }
+}
---
+++ NorthWind.Sales.Backend.UseCases/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using NorthWind.Sales.Backend.BusinessObjects.Agregates;
+global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.CreateOrder;
+global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.Repositories;
+global using NorthWind.Sales.Entities.Dtos.CreateOrder;
+global using NorthWind.Sales.Backend.UseCases.CreateOrder;

aeb3a8f15ead3bc5b87c954ae4ae14786652c8ad
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-07 10:44:01 +0800

    agregado un orderagregate

--- NorthWind.Sales.Backend.BusinessObjects/Agregates/OrderAgregate.cs
+++ NorthWind.Sales.Backend.BusinessObjects/Agregates/OrderAgregate.cs
@@ -1,14 +1,33 @@
-﻿using NorthWind.Sales.Backend.BusinessObjects.POCOEntities;
-using NorthWind.Sales.Backend.BusinessObjects.ValueObjects;
-
-namespace NorthWind.Sales.Backend.BusinessObjects.Agregates;
+﻿namespace NorthWind.Sales.Backend.BusinessObjects.Agregates;
 public class OrderAgregate : Order
 {
     readonly List<OrderDetail> OrderDetailsField = [];
     public IReadOnlyCollection<OrderDetail> OrderDetails => OrderDetailsField;
     public void AddDetail(int productId, decimal unitPrice, short quantity)
     {
-
+        OrderDetail existingOrderDetail = OrderDetailsField.FirstOrDefault(o=> o.ProductId == productId);
+        if (existingOrderDetail != default)
+        {
+            quantity += existingOrderDetail.Quantity;
+            OrderDetailsField.Remove(existingOrderDetail);
+        }
+        OrderDetailsField.Add(new OrderDetail(productId, unitPrice, quantity));
     }
 
+    public static OrderAgregate From(CreateOrderDto orderDto)
+    {
+        OrderAgregate orderAgregate = new OrderAgregate
+        {
+            CustomerId = orderDto.CustomerId,
+            ShipAddress = orderDto.ShipAddress,
+            ShipCity = orderDto.ShipCity,
+            ShipCountry = orderDto.ShipCountry,
+            ShipPostalcode = orderDto.ShipPostalcode            
+        };
+        foreach (CreateOrderDetailDto item in orderDto.OrderDetails)
+        {
+            orderAgregate.AddDetail(item.ProductId, item.UnitPrice, item.Quantity);
+        }
+        return orderAgregate;
+    }
 }

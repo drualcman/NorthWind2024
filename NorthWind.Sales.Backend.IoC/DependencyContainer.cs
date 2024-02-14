0e2dab67608d635927ec1c469b086397a1e8012d
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-14 10:29:29 +0800

    agregado un projecto de nversion de control

---
+++ NorthWind.Sales.Backend.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+public static class DependencyContainer
+{
+    public static IServiceCollection AddBackendServices(this IServiceCollection services,
+        Action<DbOptions> configureDbOptions)
+    {
+        services
+            .AddSalesUseCases()
+            .AddRepositories()
+            .AddDataContexts(configureDbOptions)
+            .AddPresenters();
+        return services;
+    }
+}

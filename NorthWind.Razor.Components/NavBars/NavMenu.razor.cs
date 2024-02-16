68d56b63a42cee1493a0004269a289f4e26f1865
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-16 12:03:59 +0800

    agregado componetes genericos

---
+++ NorthWind.Razor.Components/NavBars/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NorthWind.Razor.Components.NavBars;
+public partial class NavMenu
+{
+    [Parameter] public RenderFragment NavBarBrand { get; set; }
+    [Parameter] public RenderFragment NavBarItems { get; set; }
+
+    bool CollapseNaveMenu = true;
+    string NavMenuCssClass => CollapseNaveMenu ? "collapse" : null;
+
+    void ToggleNavMenu()
+    {
+        CollapseNaveMenu = !CollapseNaveMenu;
+    }
+}

e03580690bc6048b587c88917ae7b28efdcbaf6d
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-21 11:04:37 +0800

    agregado el main layout

---
+++ NorthWind.Sales.Frontend.Views/DependencyContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+public static class DependencyContainer
+{
+    public static IServiceCollection AddViewsServices(this IServiceCollection services)
+    {
+        services.AddScoped<CreateOrderViewModel>();
+        return services;
+    }
+}
--- NorthWind.Sales.Frontend.Views/GlobalUsings.cs
+++ NorthWind.Sales.Frontend.Views/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using NorthWind.Sales.Frondend.BusinessObjects.Interfaces;
 global using NorthWind.Sales.Frontend.Views.Resources;
 global using Microsoft.AspNetCore.Components;
 global using NorthWind.Sales.Frontend.Views.ViewModels.CreateOrder;
+global using Microsoft.AspNetCore.Components.Web;

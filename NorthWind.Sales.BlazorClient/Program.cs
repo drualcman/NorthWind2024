128655949772af4a2fbcb099c90542e8661d5984
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-21 11:17:27 +0800

    agregado el cliente

---
+++ NorthWind.Sales.BlazorClient/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddNorthWindSalesServices(client => client.BaseAddress = new Uri(builder.Configuration["WebApiAddress"]));
+
+await builder.Build().RunAsync();

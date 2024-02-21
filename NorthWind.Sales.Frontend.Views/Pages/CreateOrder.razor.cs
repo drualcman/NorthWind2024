3c413d85552f332810e187380e1c633f6ba56905
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-21 11:04:23 +0800

    agregaga la pagina de create order

---
+++ NorthWind.Sales.Frontend.Views/Pages/CreateOrder.razor.cs
@@ -0,0 +1,12 @@
+namespace NorthWind.Sales.Frontend.Views.Pages;
+public partial class CreateOrder
+{
+    [Inject] CreateOrderViewModel ViewModel { get; set; }
+
+    ErrorBoundary ErrorBoundaryRef;
+
+    void Recover()
+    {
+        ErrorBoundaryRef?.Recover();
+    }
+}

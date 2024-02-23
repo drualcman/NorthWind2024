1a8807bb387d08639a71946558d41619fca3474c
Sergi Ortiz Gómez <drualcman@msn.com>
2024-02-23 09:55:49 +0800

    agregado el cliente maui

---
+++ NorthWind.Sales.MauiClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthWind.Sales.MauiClient;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ NorthWind.Sales.MauiClient/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthWind.Sales.MauiClient;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}

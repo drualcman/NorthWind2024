2611b2d918d58030776f82a75b567eab9f9b565b
Sergi Ortiz Gómez <drualcman@msn.com>
2024-03-15 10:57:34 +0800

    finalizando con las excepcoines

--- NorthWindExceptions.Entities/ExceptionHandlers/ExceptionHandlerOrchestrator.cs
+++ NorthWindExceptions.Entities/ExceptionHandlers/ExceptionHandlerOrchestrator.cs
@@ -1,9 +1,41 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+
 namespace NorthWindExceptions.Entities.ExceptionHandlers;
 internal class ExceptionHandlerOrchestrator : Microsoft.AspNetCore.Diagnostics.IExceptionHandler
 {
-    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    readonly Dictionary<Type, object> Handlers;
+
+    public ExceptionHandlerOrchestrator(
+        [FromKeyedServices(typeof(IExceptionHandler<>))]
+        IEnumerable<object> handlers)
     {
-        throw new NotImplementedException();
+        Handlers = [];
+        foreach (var handler in handlers)
+        {
+            Type exceptionType = handler.GetType()
+                .GetInterfaces()
+                .First(i=> i.IsGenericType && 
+                           i.GetGenericTypeDefinition() == typeof(IExceptionHandler<>))
+                .GetGenericArguments()[0];
+
+            Handlers.TryAdd(exceptionType, handler);
+        }
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        bool handled = false;
+
+        if(Handlers.TryGetValue(exception.GetType(), out object handler))
+        {
+            Type handlerType = handler.GetType();
+            ProblemDetails details = (ProblemDetails)handlerType
+                .GetMethod(nameof(IExceptionHandler<Exception>.Handle))
+                .Invoke(handler, new object[] { exception } );
+            await httpContext.WriteProblemDetailsAsync(details);
+            handled = true;
+        }
+
+        return handled;
     }
 }
